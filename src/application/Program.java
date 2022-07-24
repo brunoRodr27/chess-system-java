c54e0eaa0408d82c4e1c16799433d09e3f7269ba
Bruno Rodrigues <brunodasilvarodrigues.28@gmail.com>
2022-07-24 00:04:15 -0300

    Promoção

--- src/application/Program.java
+++ src/application/Program.java
@@ -38,6 +38,13 @@ public class Program {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Escolha a peca da promocao B | N | R | Q: ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
